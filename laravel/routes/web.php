e218d311884501a0812f77fb4465dd3df184aba8
tnurhidayah24 <tnurhidayah24@gmail.com>
2022-02-05 07:44:45 +0700

    menampilkan kata

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/anak-males', function() {
+    return '<p>Halo Anak Males</p>';
+});
